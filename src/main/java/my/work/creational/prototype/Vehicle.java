6439ea3d142d8e7d48814c9182649d72c9e7e28d
Admin <Admin@host.docker.internal>
2025-01-01 23:40:16 +0200

    Prototype design pattern

---
+++ src/main/java/my/work/creational/prototype/Vehicle.java
@@ -0,0 +1,22 @@
+package my.work.creational.prototype;
+
+public interface Vehicle {
+
+	enum VehicleType {
+		CAR, VAN, TRUCK
+	}
+
+	enum Color {
+		RED, GREEN, BLUE, BLACK, WHITE
+	}
+
+	enum Engine {
+		PETROL, DIESEL, ELECTRIC
+	}
+
+	void setEngine(Engine engine);
+
+	void setColor(Color color);
+
+	Vehicle clone(Color color);
+}
